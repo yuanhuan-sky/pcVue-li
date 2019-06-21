131e66e028fdfb04a05bd7cc51a7ba5bcb4df2be
lipengzhou <lipengzhou@itcast.cn>
2019-06-21 12:23:26 +0800

    用户登录-发送登录请求完成基本登录流程

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -19,7 +19,7 @@
           </el-col>
         </el-form-item>
         <el-form-item>
-          <el-button class="btn-login" type="primary" @click="onSubmit">登录</el-button>
+          <el-button class="btn-login" type="primary" @click="handleLogin">登录</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -42,8 +42,25 @@ export default {
   },
 
   methods: {
-    onSubmit () {
-      console.log('submit!')
+    handleLogin () {
+      axios({
+        method: 'POST',
+        url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
+        data: this.form
+      })
+        .then(res => { // >=200 && < 400 的状态码会进入 then 成功
+          console.log(res.data)
+          this.$message({
+            message: '登录成功',
+            type: 'success'
+          })
+          this.$router.push({
+            name: 'home'
+          })
+        })
+        .catch((e) => {
+          this.$message.error('登录失败，手机号或验证码错误')
+        }) // >= 400 的状态码都会进入这里
     },
 
     handleSendCode () {
