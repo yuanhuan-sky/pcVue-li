3e6b38aa8597c0175a35bf74c1764f5570b500fc
lipengzhou <lipengzhou@itcast.cn>
2019-06-21 09:17:01 +0800

    安装并配置Element

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 // 先找文件，没有就找目录
 // 如果找到目录，优先加载目录中的 index
 import router from './router'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
