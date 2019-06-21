50c62c25494c87c8b17bc10f2a432b7c73c4fa19
lipengzhou <lipengzhou@itcast.cn>
2019-06-21 10:15:15 +0800

    添加home和login路由组件&配置路由导航

--- src/router/index.js
+++ src/router/index.js
@@ -5,5 +5,18 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      name: 'home',
+      path: '/',
+      // 在整个项目中，模块路径中的 @ 表示的是 src 目录
+      // 无论你当前模块在哪里，@ 都可以直接定位到 src
+      // 加载一个目录可以默认加载它的 index.js、index.vue、index.json。。。
+      component: () => import('@/views/home')
+    },
+    {
+      name: 'login',
+      path: '/login',
+      component: () => import('@/views/login')
+    }
   ]
 })
