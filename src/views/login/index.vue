a6cdfbf9170f74378bf9d89ee6f2bdfe0b0fbaac
lipengzhou <lipengzhou@itcast.cn>
2019-06-21 11:51:35 +0800

    用户登录-发送验证码-使用极验API初始化验证码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -52,7 +52,28 @@ export default {
         method: 'GET',
         url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
       }).then(res => {
-        console.log(res.data)
+        const { data } = res.data
+        window.initGeetest({
+          // 以下配置参数来自服务端 SDK
+          gt: data.gt,
+          challenge: data.challenge,
+          offline: !data.success,
+          new_captcha: data.new_captcha,
+          product: 'bind' // 隐藏，直接弹出式
+        }, function (captchaObj) {
+          captchaObj.onReady(function () {
+            // 验证码ready之后才能调用verify方法显示验证码
+            captchaObj.verify() // 弹出验证码内容框
+          }).onSuccess(function () {
+            // your code
+            console.log(captchaObj.getValidate())
+          }).onError(function () {
+            // your code
+          })
+
+          // 在这里注册 “发送验证码” 按钮的点击事件，然后验证用户是否输入手机号以及手机号格式是否正确，没有问题：
+          // captchaObj.verify
+        })
       })
     }
   }
