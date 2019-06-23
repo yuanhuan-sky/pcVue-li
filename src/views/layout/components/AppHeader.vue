64d653bb58e281c525bbfb15663ff62bd51a1211
lipengzhou <lipengzhou@itcast.cn>
2019-06-23 12:15:48 +0800

    Layout组件-头部布局

--- src/views/layout/components/AppHeader.vue
+++ src/views/layout/components/AppHeader.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>
-    <p>AppHeader Component</p>
-  </div>
+  <el-row :gutter="20">
+    <el-col :span="10">江苏传智播客教育科技股份有限公司</el-col>
+    <el-col :span="5" :offset="5">
+      <el-dropdown>
+        <img width="30" src="http://toutiao.meiduo.site/Fkj6tQi3xJwVXi1u2swCElotfdCi">
+        <span class="el-dropdown-link">
+          下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>用户设置</el-dropdown-item>
+          <el-dropdown-item>退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
