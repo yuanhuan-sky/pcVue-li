6024f120506ddc1fae4e45cde8d57c8861878dde
lipengzhou <lipengzhou@itcast.cn>
2019-06-24 10:08:17 +0800

    用户退出

--- src/views/layout/components/AppHeader.vue
+++ src/views/layout/components/AppHeader.vue
@@ -2,14 +2,18 @@
   <el-row class="app-header" :gutter="20">
     <el-col :span="10">江苏传智播客教育科技股份有限公司</el-col>
     <el-col :span="5" :offset="5">
-      <el-dropdown trigger="click">
+      <el-dropdown trigger="click" @command="handleCommand">
         <span class="el-dropdown-link">
           <img width="30" :src="userInfo.photo">
           {{ userInfo.name }}<i class="el-icon-arrow-down el-icon--right"></i>
         </span>
         <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>用户设置</el-dropdown-item>
-          <el-dropdown-item>退出</el-dropdown-item>
+          <el-dropdown-item command="a">用户设置</el-dropdown-item>
+          <!--
+            我们可以使用 .native 事件修饰符将原始的 html 页面注册到组件的根元素
+           -->
+          <!-- <el-dropdown-item command="b">退出</el-dropdown-item> -->
+          <el-dropdown-item @click.native="handleLogout">退出</el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
     </el-col>
@@ -28,6 +32,35 @@ export default {
     this.userInfo = JSON.parse(window.localStorage.getItem('user_info')) || {
       name: '123456798',
       photo: 'http://toutiao.meiduo.site/Fkj6tQi3xJwVXi1u2swCElotfdCi'
+    }
+  },
+  methods: {
+    handleLogout () {
+      this.$confirm('确认退出吗？', '退出提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // 清空本地存储中的 user_info
+        window.localStorage.removeItem('user_info')
+
+        // 跳转到登录页
+        this.$router.push({ name: 'login' })
+
+        this.$message({
+          type: 'success',
+          message: '退出成功!'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消退出'
+        })
+      })
+    },
+
+    handleCommand (dropdownItem) {
+      console.log('handleCommand', dropdownItem)
     }
   }
 }
