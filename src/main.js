8a101f7d15b43e33f894c5171e9871a748ee7baa
lipengzhou <lipengzhou@itcast.cn>
2019-06-26 10:29:54 +0800

    接口访问权限-使用请求拦截器统一添加token

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'nprogress/nprogress.css'
 import axios from 'axios'
+import { getUser } from '@/utils/auth'
 
 // 先找文件，没有就找目录
 // 如果找到目录，优先加载目录中的 index
@@ -18,6 +19,34 @@ import './styles/index.less'
 // 路径最后的 /，多退少补
 axios.defaults.baseURL = 'http://toutiao.course.itcast.cn/mp/v1_0/'
 // axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+
+/**
+ * Axios 请求拦截器：axios 发出去的请求会先经过这里
+ * config 是本次请求相关的配置对象
+ */
+axios.interceptors.request.use(config => {
+  const user = getUser()
+
+  // 如果有 user 数据，则往本次请求中添加用户 token
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+
+  // return config 是允许请求发送的开关
+  // 我们可以在这之前进行一些业务逻辑操作
+  return config
+}, error => {
+  return Promise.reject(error)
+})
+
+/**
+ * Axios 响应拦截器：axios 收到的响应会先经过这里
+ */
+axios.interceptors.response.use(response => {
+  return response
+}, error => {
+  return Promise.reject(error)
+})
 
 Vue.use(ElementUI)
 
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -73,6 +73,23 @@ export default {
         address: '上海市普陀区金沙江路 1516 弄'
       }]
     }
+  },
+
+  created () {
+    this.loadArticles()
+  },
+
+  methods: {
+    async loadArticles () {
+      // 除了登录相关接口之后，其它接口都必须在请求头中通过 Authorization 字段提供用户 token
+      // 当我们登录成功，服务端会生成一个 token 令牌，放到用户信息中
+      const res = await this.$http({
+        method: 'GET',
+        url: '/articles'
+      })
+
+      console.log(res.data)
+    }
   }
 }
 </script>
