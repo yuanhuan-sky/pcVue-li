b9b06b7da8e35e0427bd448e15fb5c05604ef4dc
lipengzhou <lipengzhou@itcast.cn>
2019-06-26 11:09:51 +0800

    响应拦截器-统一处理401无效token

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'nprogress/nprogress.css'
 import axios from 'axios'
-import { getUser } from '@/utils/auth'
+import { getUser, removeUser } from '@/utils/auth'
 
 // 先找文件，没有就找目录
 // 如果找到目录，优先加载目录中的 index
@@ -42,7 +42,7 @@ axios.interceptors.request.use(config => {
 /**
  * Axios 响应拦截器：axios 收到的响应会先经过这里
  */
-axios.interceptors.response.use(response => {
+axios.interceptors.response.use(response => { // >= 200 && < 400 的状态码会进入这里
   // response 就是响应结果对象
   // 这里将 response 原样返回，那么你发请求的地方收到的就是 response
   // 我们可以控制请求收到的响应数据格式
@@ -51,7 +51,17 @@ axios.interceptors.response.use(response => {
   } else {
     return response.data
   }
-}, error => {
+}, error => { // >= 400 的状态码会进入这里
+  // 如果用户 token 无效，让其跳转到登录页面
+  if (error.response.status === 401) {
+    // 清除本地存储中的无效 token 的用户信息
+    removeUser()
+
+    // 跳转到用户登录页面
+    router.push({
+      name: 'login'
+    })
+  }
   return Promise.reject(error)
 })
 
