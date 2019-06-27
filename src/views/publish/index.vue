d9cf6d64c36bd667db1c6bff0a96fc1098f6d08f
lipengzhou <lipengzhou@itcast.cn>
2019-06-27 11:38:31 +0800

    发布文章-布局

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,17 +1,67 @@
 <template>
-  <div>
-    <p>Public Component</p>
-  </div>
+  <el-card class="publish-card">
+    <div slot="header" class="header">
+      <span>发布文章</span>
+      <div>
+        <el-button type="success">发布</el-button>
+        <el-button type="primary">存入草稿</el-button>
+      </div>
+    </div>
+    <el-row>
+      <el-col :span="10">
+        <!-- 表单 -->
+        <el-form ref="form" :model="articleForm" label-width="80px">
+          <el-form-item label="标题">
+            <el-input v-model="articleForm.title"></el-input>
+          </el-form-item>
+          <el-form-item label="内容">
+            <el-input type="textarea" v-model="articleForm.content"></el-input>
+          </el-form-item>
+          <el-form-item label="封面">
+            <!-- <el-radio-group>
+              <el-radio label="线上品牌商赞助"></el-radio>
+              <el-radio label="线下场地免费"></el-radio>
+            </el-radio-group> -->
+          </el-form-item>
+          <el-form-item label="频道">
+            <el-select v-model="articleForm.channel_id" placeholder="请选择活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+        <!-- /表单 -->
+      </el-col>
+    </el-row>
+  </el-card>
 </template>
 
 <script>
 export default {
   name: 'AppPublish',
   data () {
-    return {}
+    return {
+      articleForm: {
+        title: '', // 标题
+        content: '', // 内容
+        channel_id: 3, // 频道
+        cover: { // 封面
+          type: 0, // 封面类型 -1:自动，0-无图，1-1张，3-3张
+          images: []
+        }
+      }
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.publish-card {
+  min-height: 100%;
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
 </style>
