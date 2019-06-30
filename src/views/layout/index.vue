1376c77fe408c8fa6abc50ef3b480a37bb1c85db
lipengzhou <lpzmail@163.com>
2019-06-30 09:37:58 +0800

    禁用路由缓存

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -12,7 +12,18 @@
       <!-- 内容 -->
       <el-main>
         <!-- Home、发布文章、文章列表、评论 ，，，， 都显示在这里 -->
-        <router-view/>
+
+        <!--
+          禁用路由缓存，只需要给 router-view 添加一个唯一的 key 即可
+          这样路由就不会去缓存组件了
+          这个 key 要确保唯一
+
+          禁用路由换粗不是必须的，看情况
+          如果你要刻意使用默认的路由缓存能力，那么就不要禁用，去呗缓存的路由中通过
+            方式一：监视 $route 做处理
+            方式二：2.2 新增的 beforeRouteUpdate 路由导航钩子做处理
+         -->
+        <router-view :key="$route.path"/>
       </el-main>
     </el-container>
   </el-container>
--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -69,6 +69,7 @@ export default {
     ArticleChannel,
     quillEditor
   },
+
   data () {
     return {
       articleForm: {
@@ -83,6 +84,35 @@ export default {
       editorOption: {} // 富文本编辑器配置选项
     }
   },
+
+  /**
+   * 监视
+   * 可以监视实例中的数据成员
+   * 当被监视数据发生变化，就会调用处理函数
+   */
+  // watch: {
+  //   // 监视实例(this)中的 $route，当 $route 发生变化，执行对于的处理函数
+  //   '$route' (to, from) {
+  //     // console.log(this.$route)
+  //     // 对路由变化作出响应...
+  //     console.log(to, from)
+
+  //     // 从编辑到发布，由于是一个组件，路由会缓存，不会重新创建
+  //     // 所以这里加一个处理：
+  //     //  对于当前这个组件来说，如果你是从编辑过来的，则将表单内容清空
+  //     if (from.name === 'publish-edit') {
+  //       this.articleForm = {
+  //         title: '', // 标题
+  //         content: '', // 内容
+  //         channel_id: '', // 频道
+  //         cover: { // 封面
+  //           type: 0, // 封面类型 -1:自动，0-无图，1-1张，3-3张
+  //           images: []
+  //         }
+  //       }
+  //     }
+  //   }
+  // },
 
   created () {
     if (this.$route.name === 'publish-edit') {
