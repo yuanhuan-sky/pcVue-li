5a58a4e8ecac36314412a440ccfea7afd2966ca7
lipengzhou <lpzmail@163.com>
2019-06-30 12:54:08 +0800

    echarts使用

--- src/router/index.js
+++ src/router/index.js
@@ -56,6 +56,11 @@ const router = new Router({
           name: 'media',
           path: '/media',
           component: () => import('@/views/media')
+        },
+        {
+          name: 'fans',
+          path: '/fans',
+          component: () => import('@/views/fans')
         }
       ]
     },
